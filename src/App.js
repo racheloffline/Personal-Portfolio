288cab0e02adde5eb31146d3a707a119b9505237
racheloffline <rachehu@uw.edu>
2023-05-25 21:01:17 -0700

    switch user

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and Rachel is da best.
+          Edit <code>src/App.js</code> one two three
         </p>
         <a
           className="App-link"
